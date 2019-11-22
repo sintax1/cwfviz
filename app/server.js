00c708d460e3fc2d5fe498012b040102a41b4380
Craig Koroscil <ckoroscil@circadence.com>
2019-11-22 12:07:24 -0500

    changed cypher query sorting

--- app/server.js
+++ app/server.js
@@ -38,7 +38,7 @@ app.get('/wr-paths', function (req, res) {
     // The Cypher query used to gather the workrole relationships. Filtering out the 6 KSAs that are common across all 52 WRs.
     var query = `MATCH (c:NICECategory)--(sa:NICESpecialtyArea)--(w1:NICEWorkrole)<-[:NICE_WORKROLE]-(k:KSAT)-[:NICE_WORKROLE]->(w2:NICEWorkrole)
     WHERE NOT k.id IN ['K0001', 'K0002', 'K0003', 'K0004', 'K0005', 'K0006']
-    RETURN c.id, sa.id, w1.title, collect(w2.title)`
+    RETURN c.id, sa.id, w1.title, collect(w2.title) ORDER BY c.id`
     var params={}
     var cb=function(err, data) {
         if (err) {
